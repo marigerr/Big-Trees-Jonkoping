f5cafea38e023f3f66459a4ea3e266b916322b81
marigerr <marigerr.sv@gmail.com>
2017-04-21 10:31:50 +0200

    geolocation with Nav and Google seem to work.  Also linting

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var topo = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFyaWdlcnIiLCJhIjoiY2l6NDgxeDluMDAxcjJ3cGozOW1tZnV0NCJ9.Eb2mDsjDBmza-uhme0TLSA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.streets'
 });
 
 var satellite = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFyaWdlcnIiLCJhIjoiY2l6NDgxeDluMDAxcjJ3cGozOW1tZnV0NCJ9.Eb2mDsjDBmza-uhme0TLSA', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.satellite'
 })
 
@@ -19,9 +19,9 @@ var baseLayers = {
 
 // var latlng = L.latLng(57.90930939999999,14.074366499999996);
 
-var map = L.map('map', {layers: [topo]});//, center: latlng, zoom: 13, zoomControl : false
+var map = L.map('map', { layers: [topo] });//, center: latlng, zoom: 13, zoomControl : false
 // L.control.zoom( {position : 'bottomright'} ).addTo(map);
-var sidebar = L.control.sidebar('sidebar', {position: 'right'}).addTo(map);
+var sidebar = L.control.sidebar('sidebar', { position: 'right' }).addTo(map);
 
 
 // lc = L.control.locate({
@@ -34,20 +34,20 @@ var geojsonLayer;
 // var progress = document.getElementById('progress');
 // var progressBar = document.getElementById('progress-bar');
 var finishedLoading;
-var markers = L.markerClusterGroup({showCoverageOnHover: false, maxClusterRadius: 50, disableClusteringAtZoom: 15, spiderfyOnMaxZoom: false}); //, chunkedLoading: true, chunkProgress :checkProgress
+var markers = L.markerClusterGroup({ showCoverageOnHover: false, maxClusterRadius: 50, disableClusteringAtZoom: 15, spiderfyOnMaxZoom: false }); //, chunkedLoading: true, chunkProgress :checkProgress
 // var currentData;
-$.getJSON("./GeoJson/Jönköping.geojson", function(data){
+$.getJSON("./GeoJson/Jönköping.geojson", function (data) {
     success(data);
     // geojsonLayer.addData(HaboTrees, {pointToLayer: pointToLayer, onEachFeature: onEachFeature});
     // console.log(geojsonLayer._layers);
     // map.fitBounds(geojsonLayer.getBounds());
 });
 
-function success(data){
-    geojsonLayer = L.geoJSON(data, {pointToLayer: pointToLayer, onEachFeature: onEachFeature});
+function success(data) {
+    geojsonLayer = L.geoJSON(data, { pointToLayer: pointToLayer, onEachFeature: onEachFeature });
     markers.addLayer(geojsonLayer);
     map.addLayer(markers);
-	map.fitBounds(markers.getBounds());
+    map.fitBounds(markers.getBounds());
 
     // console.log(markers);
 }
@@ -56,35 +56,36 @@ function success(data){
 // map.addLayer(markers);
 // map.setView(geojsonLayer.getBounds().getCenter());
 
-L.control.layers(baseLayers,{},{position : 'topleft'}).addTo(map);
+L.control.layers(baseLayers, {}, { position: 'topleft' }).addTo(map);
 
 // $("#dateInput").focusout( function(e) {
 //     console.log(e.target.value);
 //     getComplaints(e.target.value);  
 // })
 
-$( "#kommunSel" ).change(function(e) {
-//   alert( $("#complaintDD option:selected"));
-//   console.log(e.target.value);
+$("#kommunSel").change(function (e) {
+    //   alert( $("#complaintDD option:selected"));
+    //   console.log(e.target.value);
     $("#circumferenceSel").val(1);
     // $("#circumferenceSel option:selected")
     updateMap(e.target.value);
     sidebar.close();
 });
 
-$( "#circumferenceSel" ).change(function(e) {
-//   alert( $("#complaintDD option:selected"));
+$("#circumferenceSel").change(function (e) {
+    //   alert( $("#complaintDD option:selected"));
     $("#noResults").hide();
     console.log(e.target.value);
     filterMap(e.target.value, $('#kommunSel').find(":selected").text());
 });
 
-$( "#locateBtn" ).click(function(e) {
-//   alert( $("#complaintDD option:selected"));
+$("#locateBtn").click(function (e) {
+    //   alert( $("#complaintDD option:selected"));
     // $("#noResults").hide();
     // console.log(e.target.value);
     // filterMap(e.target.value, $('#kommunSel').find(":selected").text());
-    findLocation();
+    // findLocationWithNavigator();
+    findLocationWithGoogleGeolocation();
 });
 
 
@@ -127,25 +128,25 @@ $( "#locateBtn" ).click(function(e) {
 // })
 
 function pointToLayer(feature, latlng) {
-  
+
     var radius;
     var x = feature.properties.Stamomkret;
     switch (true) {
-    case (x < 1000):
-        // console.log("less than six hundred");
-        radius = 5;
-        break;
-    case (x >= 1000  && x < 1500):
-        // alert("between 5 and 8");
-        radius = 10;
-        break;
-    case (x >= 1500):
-        // alert("between 9 and 11");
-        radius = 15;
-        break;
-    default:
-        console.log("error with radius");
-        break;
+        case (x < 1000):
+            // console.log("less than six hundred");
+            radius = 5;
+            break;
+        case (x >= 1000 && x < 1500):
+            // alert("between 5 and 8");
+            radius = 10;
+            break;
+        case (x >= 1500):
+            // alert("between 9 and 11");
+            radius = 15;
+            break;
+        default:
+            console.log("error with radius");
+            break;
     }
     return new L.CircleMarker(latlng, {
         radius: radius,
@@ -163,68 +164,70 @@ function pointToLayer(feature, latlng) {
 
 function updateMap(selection) {
     var url = "./GeoJson/" + selection + ".geojson";
-    $.getJSON(url, function(data){
+    $.getJSON(url, function (data) {
         var HaboTrees = data;
-        
+
         /* Rest of the code with uses the "ajavascriptjsonvariable" variable */
-        if(geojsonLayer){
+        if (geojsonLayer) {
             console.log(geojsonLayer);
             markers.removeLayer(geojsonLayer);
             // geojsonLayer.remove();
-            geojsonLayer ={};
+            geojsonLayer = {};
 
-        }     
-            console.log("hi");
-           
-        geojsonLayer = L.geoJSON(HaboTrees, {pointToLayer: pointToLayer, onEachFeature: onEachFeature});
+        }
+        console.log("hi");
+
+        geojsonLayer = L.geoJSON(HaboTrees, { pointToLayer: pointToLayer, onEachFeature: onEachFeature });
         markers.addLayer(geojsonLayer);
         map.addLayer(markers);
         // if (finishedLoading) {
-    	map.fitBounds(markers.getBounds());
+        map.fitBounds(markers.getBounds());
         // }
         // map.fitBounds(geojsonLayer.getBounds());
 
     });
 }
 
-function filterMap(sizeFilter, kommun){
+function filterMap(sizeFilter, kommun) {
 
     var url = "./GeoJson/" + kommun + ".geojson";
-    $.getJSON(url, function(data){
+    $.getJSON(url, function (data) {
         var currentTrees = data;
-        if(geojsonLayer){
+        if (geojsonLayer) {
             markers.removeLayer(geojsonLayer);
             // geojsonLayer.remove();
-            geojsonLayer ={};
-        } 
-        if (sizeFilter == 1){
+            geojsonLayer = {};
+        }
+        if (sizeFilter == 1) {
             console.log("return all points");
-            geojsonLayer = L.geoJSON(currentTrees, {pointToLayer: pointToLayer, onEachFeature: onEachFeature});       
+            geojsonLayer = L.geoJSON(currentTrees, { pointToLayer: pointToLayer, onEachFeature: onEachFeature });
         } else {
-            geojsonLayer = L.geoJSON(currentTrees, 
-                                    {pointToLayer: pointToLayer, 
-                                    filter: function(feature, layer) {
-                                                console.log(sizeFilter);
-                                                if(sizeFilter == 5){
-                                                    return feature.properties.Stamomkret < 1000 
-                                                } else if (sizeFilter == 10){
-                                                    return feature.properties.Stamomkret > 1000 && feature.properties.Stamomkret < 1500
-                                                } else if (sizeFilter == 15){
-                                                    return feature.properties.Stamomkret > 1500
-                                                };
-                                                // return feature.properties.Stamomkret ;
-                                                // return feature.properties.Stamomkret > 1000
-                                            },
-                                    onEachFeature: onEachFeature}
-                                    );
+            geojsonLayer = L.geoJSON(currentTrees,
+                {
+                    pointToLayer: pointToLayer,
+                    filter: function (feature, layer) {
+                        console.log(sizeFilter);
+                        if (sizeFilter == 5) {
+                            return feature.properties.Stamomkret < 1000
+                        } else if (sizeFilter == 10) {
+                            return feature.properties.Stamomkret > 1000 && feature.properties.Stamomkret < 1500
+                        } else if (sizeFilter == 15) {
+                            return feature.properties.Stamomkret > 1500
+                        };
+                        // return feature.properties.Stamomkret ;
+                        // return feature.properties.Stamomkret > 1000
+                    },
+                    onEachFeature: onEachFeature
+                }
+            );
         }
         console.log(geojsonLayer._layers);
-        if (!jQuery.isEmptyObject(geojsonLayer._layers)){
+        if (!jQuery.isEmptyObject(geojsonLayer._layers)) {
             console.log(geojsonLayer);
             markers.addLayer(geojsonLayer);
             map.addLayer(markers);
-            map.fitBounds(markers.getBounds()); 
-            sidebar.close();           
+            map.fitBounds(markers.getBounds());
+            sidebar.close();
             // map.fitBounds(geojsonLayer.getBounds());    
         } else {
             console.log("no results")
@@ -235,21 +238,21 @@ function filterMap(sizeFilter, kommun){
 
 
 function onEachFeature(feature, layer) {
-        // console.log(feature);
-		var popupContent = ""
-		if (feature.properties) {
-            popupContent += "Id: " + feature.properties.Obj_idnr + "</br>";
-			popupContent += "Stamomkret: " + feature.properties.Stamomkret + " cm</br>";
-            popupContent += "Tradslag: " + feature.properties.Tradslag + "</br>";
-            popupContent += "Status: " + feature.properties.Tradstatus + "</br>";            
-		}
-		layer.bindPopup(popupContent);
-        // markers.addLayer(layer);
+    // console.log(feature);
+    var popupContent = ""
+    if (feature.properties) {
+        popupContent += "Id: " + feature.properties.Obj_idnr + "</br>";
+        popupContent += "Stamomkret: " + feature.properties.Stamomkret + " cm</br>";
+        popupContent += "Tradslag: " + feature.properties.Tradslag + "</br>";
+        popupContent += "Status: " + feature.properties.Tradstatus + "</br>";
+    }
+    layer.bindPopup(popupContent);
+    // markers.addLayer(layer);
 }
 
-function makeAjaxCall(url, data, type, datatype, success, error){
+function makeAjaxCall(url, data, type, datatype, success, error) {
     $.ajax({
-        url: url,     
+        url: url,
         data: data,
         type: type,
         dataType: datatype,
@@ -259,39 +262,105 @@ function makeAjaxCall(url, data, type, datatype, success, error){
 }
 
 
-function findLocation() {
+function findLocationWithNavigator() {
+    console.log("findlocation navigator function called");
     if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-          
-            console.log(lat);
-            console.log(lng);
-            
-            map.setView(L.latLng(lat, lng), 15);
-            sidebar.close(); 
 
-            // infoWindow.setPosition(pos);
-            // infoWindow.setContent('Location found.');
-            // infoWindow.open(map);
-            // map.setCenter(pos);
-          })
-        // , function() {
-        //     handleLocationError(true, infoWindow, map.getCenter());
-        //   });
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+
+        function success(pos) {
+            var crd = pos.coords;
+
+            console.log('Your current position is:');
+            console.log(`Latitude : ${crd.latitude}`);
+            console.log(`Longitude: ${crd.longitude}`);
+            console.log(`Accuracy ${crd.accuracy} meters.`);
+            map.setView(L.latLng(crd.latitude, crd.longitude), 15);
+            sidebar.close();            
+        };
+
+        function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+            findLocationWithGoogleGeolocation();
+
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, options);        
     } else {
         console.log("Browser doesn't support Geolocation");
-        $("#noResults").show();
-          // Browser doesn't support Geolocation
+        findLocationWithGoogleGeolocation();
+
+        // $("#noResults").show();
+        // Browser doesn't support Geolocation
         //   handleLocationError(false, infoWindow, map.getCenter());
-        }
+        // }
+    }
+}
+
+function findLocationWithGoogleGeolocation() {
+    console.log("google geolcation called")
+    var url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyALDj8UcNZ1fQlXcoMlJ84lSavkcyODExI";
+    var type = "POST";
+    var data;
+    var datatype = "json";
+    var success = function (response) {
+        console.log(response);
+        var lat = response.location.lat;
+        var lng = response.location.lng;
+        console.log("Accuracy: " + response.accuracy + " meters");
+
+        map.setView(L.latLng(lat, lng), 14);
+        var userLocation = "latlng=" + lat + "," + lng;
+        // latlng=40.714224,-73.961452
+        determineRegion(userLocation);
+        sidebar.close();
+    };
+    var error = function (xhr) {
+        console.log(xhr.statusText)
+    };
+
+    makeAjaxCall(url, data, type, datatype, success, error);
+}
+
+function determineRegion(userLocation) {
+    var GoogleKey = "AIzaSyALDj8UcNZ1fQlXcoMlJ84lSavkcyODExI";
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?" + userLocation + "&key=" + GoogleKey;
+    var type = "POST";
+    var data;
+    var datatype = "json";
+    var success = function (response) {
+        console.log(response);
+        console.log(response.results[4].address_components[0].short_name);
+        var region = response.results[4].address_components[0].short_name;
+        // var strArr = region.split(" ");
+        // console.log(strArr[0]);
+        updateMap(region);
+        $("#kommunSel").val(region);
+
+
+        // var lat = response.location.lat;
+        // var lng = response.location.lng;
+        // console.log("Accuracy: " + response.accuracy + " meters");
+
+        // map.setView(L.latLng(lat, lng), 14);
+        // sidebar.close();
+    };
+    var error = function (xhr) {
+        console.log(xhr.statusText)
+    };
+
+    makeAjaxCall(url, data, type, datatype, success, error);
 }
 
 // function onAccuratePositionProgress (e) {
 //     console.log(e.accuracy);
 //     console.log(e.latlng);
 //     map.setView(L.latLng(e.latlng), 15);
-    
+
 // }
 
 // function onAccuratePositionFound (e) {
