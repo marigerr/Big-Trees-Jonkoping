9be8d6882bcd356150895ec889d632b4d50bf648
marigerr <marigerr.sv@gmail.com>
2017-05-07 08:19:11 +0200

    moved color to main masterTreeArray, changed getColor

--- src/components/map/getColor.js
+++ src/components/map/getColor.js
@@ -1,76 +1,13 @@
+import {trees} from 'Data/models/treetype.js';
+
 export default function getColor(treeType){
-    treeType = treeType.toLowerCase();
-    return treeType == "Al".toLowerCase() ? 	"#0C2CAB" :
-    treeType == "Gråal".toLowerCase() ? 	"#0C2CAB" :
-    treeType == "Klibbal".toLowerCase() ? 	"#0C2CAB" :
-    treeType == "Apel".toLowerCase() ? 	"#EB8AAE" :
-    treeType == "Apel-släktet".toLowerCase() ? 	"#EB8AAE" :
-    treeType == "Apel, äpple".toLowerCase() ? 	"#EB8AAE" :
-    treeType == "Malus sp".toLowerCase() ? 	"#EB8AAE" :
-    treeType == "Vildapel".toLowerCase() ? 	"#EB8AAE" :
-    treeType == "Äpple".toLowerCase() ? 	"#EB8AAE" :
-    treeType == "Ask".toLowerCase() ? 	"#732e00" :
-    treeType == "Asp".toLowerCase() ? 	"#3E1380" : 
-    treeType == "Blodbok".toLowerCase() ? 	"#424E82" :
-    treeType == "Bok".toLowerCase() ? 	"#424E82" :
-    treeType == "Björk".toLowerCase() ? 	"#090C66" :
-    treeType == "Björk-släktet".toLowerCase() ? 	"#090C66" :
-    treeType == "Glasbjörk".toLowerCase() ? 	"#090C66" :
-    treeType == "Masurbjörk".toLowerCase() ? 	"#090C66" :
-    treeType == "Vårtbjörk".toLowerCase() ? 	"#090C66" :
-    treeType == "Fågelbär".toLowerCase() ? 	"#F0469D" :
-    treeType == "Hägg".toLowerCase() ? 	"#F0469D" :
-    treeType == "Körsbär".toLowerCase() ? 	"#F0469D" :
-    treeType == "Sötkörsbär / fågelbär".toLowerCase() ? 	"#F0469D" :
-    treeType == "Kastanj".toLowerCase() ? 	"#F56B45" :
-    treeType == "Hästkastanj".toLowerCase() ? 	"#F56B45" :
-    treeType == "Alm".toLowerCase() ? 	"#F72530" :
-    treeType == "Alm-släktet".toLowerCase() ? 	"#F72530" :
-    treeType == "Lundalm".toLowerCase() ? 	"#F72530" :
-    treeType == "Skogsalm".toLowerCase() ? 	"#F72530" :
-    treeType == "Gran".toLowerCase() ? 	"#00802b" :
-    treeType == "Ädelgran".toLowerCase() ? 	"#00802b" :
-    treeType == "Hagtornsläktet".toLowerCase() ? 	"#CF3C72" :
-    treeType == "Trubbhagtorn".toLowerCase() ? 	"#CF3C72" :
-    treeType == "Hassel".toLowerCase() ? 	"#0738FA" :
-    treeType == "Avenbok".toLowerCase() ? 	"#134D80" :
-    treeType == "En".toLowerCase() ? 	"#002b40" :
-    treeType == "Kungsen".toLowerCase() ? 	"#002b40" :
-    treeType == "Lärk".toLowerCase() ? 	"#00b33c" :
-    treeType == "Lärk, europeisk".toLowerCase() ? 	"#00b33c" :
-    treeType == "Bohuslind".toLowerCase() ? 	"#ffaa00" :
-    treeType == "Lind".toLowerCase() ? 	"#ffaa00" :
-    treeType == "Lind-släktet".toLowerCase() ? 	"#ffaa00" :
-    treeType == "Parklind".toLowerCase() ? 	"#ffaa00" :
-    treeType == "Skogslind".toLowerCase() ? 	"#ffaa00" :
-    treeType == "Blodlönn".toLowerCase() ? 	"#FA5311" :
-    treeType == "Lönn".toLowerCase() ? 	"#FA5311" :
-    treeType == "Tysklönn".toLowerCase() ? 	"#FA5311" :
-    treeType == "Bergek".toLowerCase() ? 	"#267F99" :
-    treeType == "Ek".toLowerCase() ? 	"#267F99" :
-    treeType == "Ek-släktet".toLowerCase() ? 	"#267F99" : 
-    treeType == "Skogsek".toLowerCase() ? 	"#267F99" :
-    treeType == "Päron".toLowerCase() ? 	"#DB099C" :
-    treeType == "Svarttall".toLowerCase() ? 	"#006600" :
-    treeType == "Tall".toLowerCase() ? 	"#006600" :
-    treeType == "Balsampoppel".toLowerCase() ? 	"#49287A" :
-    treeType == "Gråpoppel".toLowerCase() ? 	"#49287A" :
-    treeType == "Poppel".toLowerCase() ? 	"#49287A" :
-    treeType == "Poppel (utom asp)".toLowerCase() ? 	"#49287A" :
-    treeType == "Populus sp".toLowerCase() ? 	"#49287A" :
-    treeType == "Pyramidpoppel".toLowerCase() ? 	"#49287A" :
-    treeType == "Rönn".toLowerCase() ? 	"#F21693" :
-    treeType == "Oxel".toLowerCase() ? 	"#F584C4" :
-    treeType == "Jolster".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Knäckepil".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Pil".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Pil (flera arter)".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Salix".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Salix (viden m.fl.)".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Salix sp".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Sälg".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Vide".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Vitpil".toLowerCase() ? 	"#8C4DAB" :
-    treeType == "Idegran".toLowerCase() ? 	"#0CE80C" :
-    "#000000";
+    var color;
+    var masterTreeArray = trees();
+    $.each(masterTreeArray, function(index, tree){
+        if (treeType.match(tree.matchWith)) {
+            color = tree.color;
+            return false;
+        }
+    });
+    return color;
 }
